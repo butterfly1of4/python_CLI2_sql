3f7918c76202acaebebc24043b79a1d2f66a2ef6
butterfly1of4 <jhouck29@gmail.com>
2020-08-14 15:26:40 -0400

    better

--- lib/main.py
+++ lib/main.py
@@ -1,77 +1,63 @@
 from peewee import *
 from models import Contacts
 
-
+    
 print("My Contacts:")
 # False = off- stop running
-# True = on- keep running
-# on = False
-print("Please choose from the following menu options: \n")
-print("For a list of All Contacts: Type 'all'; \n")
-print("To find One contact by first-name: Type the first name of the person you're looking for;\n ")
-print("To create a New contact: Type 'new'\n")
-
-menu = input("Type your choice: ")
+#True = on- keep running
+on = True
+while True:   
+        
+    def intro():
+        print("Please choose from the following menu options: \n")
+        print("1: For a list of All Contacts: Type 'all'; \n")
+        print("2: To find One contact by first-name: Type the first name of the person you're looking for;\n ")
+        print("3: To create a New contact: Type 'new'\n")
+    intro()
+    menu = input("Type your choice: ")
 
 # print(menu)
-
-
-def findAll():
-
-    all = Contacts.select()
-    for entry in all:
-        print(entry.first_name, entry.last_name, entry.number, entry.email)
-
-
+    
+    def findAll():
+        all = Contacts.select()
+        for entry in all:
+            print(entry.first_name, entry.last_name,entry.number,entry.email)
     # findAll()
-if menu == 'all':  # and on == True:
-    findAll()
-    # restart()
-
+    if menu == 'all': #and on == True:       
+        findAll()
+        # restart()
+        continue
+        # break
     # on==False
-
-# CREATE
-
-
-def createContact():
+    # intro()
+#CREATE 
+    def createContact():
     # on == True
-    first = input("First Name: ")
-    last = input("Last Name: ")
-    number = input("Phone Number: ")
-    email = input("Email: ")
-    Contacts.create(first_name=first, last_name=last,
-                    number=number, email=email)
+        first = input("First Name: ")
+        last = input("Last Name: ")
+        number = input("Phone Number: ")
+        email = input("Email: ")
+        Contacts.create(first_name = first, last_name =last, number = number, email=email)
 
+    if (menu == 'new'):
+        createContact()
+        # restart()
+        continue
 
-    # c.save()
-    # createContact()
-    # on == False
-if (menu == 'new'):
-    createContact()
-    # restart()
-
-
-# FIND ONE
-def findName():
+#FIND ONE     
+    def findName():
     # on ==True
-    person = Contacts.get(Contacts.first_name == menu)
-    print(person.first_name, person.last_name, person.number, person.email)
-
-
+        person = Contacts.get(Contacts.first_name == menu)
+        print(person.first_name, person.last_name, person.number, person.email)
     # findName()
-if menu == Contacts.first_name:
-    findName()
+    if menu == Contacts.first_name:
+        findName()
     # on == False
-    # restart()
+        # restart()  
+        continue
 
-
-# def restart():
-#     while on == False:
-#         menu == menu
-#     if  on==True:
-#         print('stop')
-# restart()
-
-
-# if menu == 'exit':
-    # on == True;
+    if menu == 'exit' or menu == 'quit' or menu == 'close':
+        on  == False
+        print("Goodbye")
+    break
+    
