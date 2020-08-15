bba8c56a9660a8a0f226063c141076d7fd44a98c
butterfly1of4 <jhouck29@gmail.com>
2020-08-15 10:20:34 -0400

    exits

--- lib/main.py
+++ lib/main.py
@@ -1,63 +1,66 @@
 from peewee import *
 from models import Contacts
 
-    
+
 print("My Contacts:")
-# False = off- stop running
-#True = on- keep running
+
 on = True
-while True:   
-        
+while on == True:
+
     def intro():
-        print("Please choose from the following menu options: \n")
+        print("\nPlease choose from the following menu options: \n")
         print("1: For a list of All Contacts: Type 'all'; \n")
         print("2: To find One contact by first-name: Type the first name of the person you're looking for;\n ")
         print("3: To create a New contact: Type 'new'\n")
+        print("4: To exit the program: Type 'exit\n")
     intro()
     menu = input("Type your choice: ")
+# EXIT CHECK
 
-# print(menu)
-    
+    def checkExit():
+        if menu == 'exit':
+            on == False
+            print("Goodbye")
+        else:
+            print('go on')
+        checkExit()
+# FIND ALL
+
     def findAll():
         all = Contacts.select()
         for entry in all:
-            print(entry.first_name, entry.last_name,entry.number,entry.email)
-    # findAll()
-    if menu == 'all': #and on == True:       
+            print(entry.first_name, entry.last_name, entry.number, entry.email)
+    if menu == 'all':
         findAll()
-        # restart()
+
         continue
-        # break
-    # on==False
-    # intro()
-#CREATE 
+
+# CREATE
     def createContact():
-    # on == True
+
         first = input("First Name: ")
         last = input("Last Name: ")
         number = input("Phone Number: ")
         email = input("Email: ")
-        Contacts.create(first_name = first, last_name =last, number = number, email=email)
-
+        Contacts.create(first_name=first, last_name=last,
+                        number=number, email=email)
     if (menu == 'new'):
         createContact()
-        # restart()
+
         continue
 
-#FIND ONE     
+# FIND ONE BY FIRST NAME
     def findName():
-    # on ==True
         person = Contacts.get(Contacts.first_name == menu)
         print(person.first_name, person.last_name, person.number, person.email)
-    # findName()
-    if menu == Contacts.first_name:
+    if menu == Contacts.first_name and menu != 'exit':
         findName()
-    # on == False
-        # restart()  
         continue
+    elif menu == 'exit':
+        break
 
-    if menu == 'exit' or menu == 'quit' or menu == 'close':
-        on  == False
-        print("Goodbye")
-    break
-    
+# EXIT
+else:
+    on == False
+    menu == 'exit'
+    print("exiting")
